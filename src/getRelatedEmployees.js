6dea648c2b77f6a122ea07a8209ec26e5c45e693
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-20 16:35:41 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((empregados) => empregados.managers.includes(id));
 }
+// a primeira função define se o id recebido esta presente dentro das chaves manegers, oque qualifica a pessoa do id como gerente.
+
+// a segunda função deve fazer duas coisas: mostrar se a pessoa é gerente ou não a partir da função de cima. E depois deve filtrar o array objeto, vendo quais objetos ali dentro possuem o id do gerente na chave maneger e depois volar com o nome dos empregados que esse gerente tem sob seus cuidados.
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const related = employees.filter((element) => element.managers.includes(managerId))
+    .map((gerenciados) => `${gerenciados.firstName} ${gerenciados.lastName}`);
+  return related;
 }
 
 module.exports = { isManager, getRelatedEmployees };
