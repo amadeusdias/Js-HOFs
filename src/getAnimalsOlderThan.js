989f9f0f8d4ae7f97f43d1a6a0b77c6a79f5d40e
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-20 15:48:02 -0300

    requisito 2 co comenarios

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const especie = species.find((animals) => animals.name === animal);
+  return especie.residents.every((idade) => idade.age >= age);
 }
+// a constante especie acha o array do animal de acordo com o nome animal dado como parametro, utilizando um find para isso.
+// o return usa um every para verificar se todos os animais da const possuem a idade minima dada pelo parametro age.
+console.log(getAnimalsOlderThan('lions', 4));
 
 module.exports = getAnimalsOlderThan;
