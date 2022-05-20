4eaf4f673af02e0a908c7dc154fe3d0693eaa2e2
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-20 15:14:48 -0300

    correções lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,5 +9,5 @@ function getSpeciesByIds(...ids) {
 // parametro rest na função a fim de poder receber um numero aleatorio de ids, transforma eles em um array.
 // se ids é indefinido volta um array vazio
 // aplicar um map nos ids, de forma a retornar todos os ids se existir mais de um. Um find logo depois, aplicado ao array species de forma a achar quais ids sao iguais ao array ids.
-
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;
