4ea58eb141b2674bb132393419dd22d1c69cf9f4
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-20 15:06:14 -0300

    primeira questão com comentarios

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+  return ids.map((id) => data.species.find((especie) => especie.id === id));
 }
+// parametro rest na função a fim de poder receber um numero aleatorio de ids, transforma eles em um array.
+// se ids é indefinido volta um array vazio
+// aplicar um map nos ids, de forma a retornar todos os ids se existir mais de um. Um find logo depois, aplicado ao array species de forma a achar quais ids sao iguais ao array ids.
 
 module.exports = getSpeciesByIds;
