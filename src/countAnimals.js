b346e134a9c461a14c4f831a716dba8b2b79acd4
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-23 15:53:03 -0300

    requisitos 5, 6 e 7

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const total = {};
+    species.forEach((animals) => {
+      total[animals.name] = animals.residents.length;
+    });
+    return total;
+  }
+  if (Object.keys(animal).length === 1) {
+    const bicho = species.find((animals) => animals.name === animal.specie);
+    return bicho.residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    const nome = species.find((animals) => animals.name === animal.specie);
+    const genero = nome.residents.filter((sexo) => sexo.sex === animal.sex);
+    return genero.length;
+  }
 }
-
+// o primeiro if deve retornar um objeto contendo o par chave/valor no formato especie/numero de residentes. Para isso utiliza um forEach que itera sobre o array Species retornando o objeto total com o par chave valor pedido.
+// o segundo if verifica se o objeto recebido como parametro possui somente uma chave, no caso smp a chave scpecie, e prossegue usando um find para filtrar o array species pelo nome da specie parametro. Retorna então o numero de animais daquela especie no logico atraves do tamanho do array residents.
+// o terceiro if verifica se o numero de chaves do obejto parametro é igual a dois, a segunda chave sendo esperada como sex. Prossegue então a usar um find para achar o animal de species que é igual a especie setada como parametro. Depois filtra os animais dessa especies pelo genero e devolve o numero de animais do sexo passado como parametro.
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,37 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('ao receber como parametro a string "count" retorna o numero de elefantes no zoologico', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+  it('ao receber um parametro diferente de string, retorna essa mensagem', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('ao receber como parametro a string "name" deve retornar um array com o nome "jeffersson"', () => {
+    const actual = handlerElephants('names');
+    const expected = 'Jefferson';
+    expect(actual).toContain(expected);
+  });
+  it('ao receber a string "averageAge" deve retornar a media das idades dos elefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toBeCloseTo(expected);
+  });
+  it('ao receber a string "location" retorna a localização dos animais', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+  it('ao receber a string "popularity como parametro deve retornar um numero maior ou igual a 5', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toBeCloseTo(expected);
+  });
+  it('ao receber a string availability deve retornar retornar um array de dias que nao contem "Monday"', () => {
+    const actual = handlerElephants('availability');
+    const expected = 'Monday';
+    expect(actual).not.toContain(expected);
+  });
+});
