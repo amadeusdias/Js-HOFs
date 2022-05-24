8284b31ee06ee949b14be0f6d5dd3bb85683c0e6
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-24 16:31:02 -0300

    tentativa dois

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -24,22 +24,35 @@ describe('Testes da função getOpeningHours', () => {
     const expected = 'The zoo is open';
     expect(actual).toEqual(expected);
   });
+
   it('ao receber os argumentos Wednesday e 09:00-PM, deve retornar uma mensgem de Zoo fechado', () => {
     const actual = getOpeningHours('Wednesday', '09:00-PM');
     const expected = 'The zoo is closed';
     expect(actual).toEqual(expected);
   });
+
   it('dispara erro com a mensagem "The day must be valid. Example: Monday", quando os argumentos não tiverem formatos validos', () => {
     const actual = ('Thu', '09:00-AM');
     expect(() => getOpeningHours(actual)).toThrow('The day must be valid. Example: Monday');
   });
+
   it('dispara um erro com a mensagem "The abbreviation must be "AM" or "PM", quando os argumentos não tiverem formatados', () => {
-    expect(() => getOpeningHours('Friday', '09:00-ZM').toThrow('The abbreviation must be "AM" or "PM"'));
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
   });
+
   it('dispara um erro para os argumentos Saturday e C9:00-AM, com a mensagem "The hour should represent a number" ', () => {
     expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
   });
+
   it('dispara um erro para os argumentos Sunday e 09:c0-AM, com a mensagem "The minutes should represent a number" ', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
   });
+
+//   it('dispara um erro para horarios fora de formatação', () => {
+//     expect(() => getOpeningHours('Tuesday', '09:78')).toThrow('The minutes must be between 0 and 59');
+//   });
+
+//   it('dispara um erro para horarios fora de formatação', () => {
+//     expect(() => getOpeningHours('Saturday', '23:00')).toThrow('The hour must be between 0 and 12');
+//   });
 });
