e9eb098c3b53cc205c03843db86a1a7dbcc651c0
Amadeus Dias <amadeus@pop-os.localdomain>
2022-05-24 16:12:42 -0300

    ve se vai

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -29,4 +29,17 @@ describe('Testes da função getOpeningHours', () => {
     const expected = 'The zoo is closed';
     expect(actual).toEqual(expected);
   });
+  it('dispara erro com a mensagem "The day must be valid. Example: Monday", quando os argumentos não tiverem formatos validos', () => {
+    const actual = ('Thu', '09:00-AM');
+    expect(() => getOpeningHours(actual)).toThrow('The day must be valid. Example: Monday');
+  });
+  it('dispara um erro com a mensagem "The abbreviation must be "AM" or "PM", quando os argumentos não tiverem formatados', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM').toThrow('The abbreviation must be "AM" or "PM"'));
+  });
+  it('dispara um erro para os argumentos Saturday e C9:00-AM, com a mensagem "The hour should represent a number" ', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
+  });
+  it('dispara um erro para os argumentos Sunday e 09:c0-AM, com a mensagem "The minutes should represent a number" ', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
+  });
 });
